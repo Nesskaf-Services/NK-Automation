6c4ac3a880a30a5fa5352433936161fd34d651aa
ikifar2012 <ikifar2012@users.noreply.github.com>
2024-01-15 22:56:46 +0000

    Update spec-report.sh to include RAM and storage information

--- bash/spec-report.sh
+++ bash/spec-report.sh
@@ -2,7 +2,7 @@
 
 # Figlet ASCII art with "NK"
 echo "======================="
-figlet "NK"
+figlet "NK Spec Report"
 echo "======================="
 
 # Get the total number of CPU cores
@@ -12,23 +12,27 @@ echo "Total CPU Cores: $cpu_core_count"
 
 # Get total RAM
 total_ram=$(free -m | awk '/^Mem:/{print $2}')
+total_used_ram=$(free -m | awk '/^Mem:/{print $3}')
 
 echo "Total RAM: ${total_ram}MB"
+echo "Total Used RAM: ${total_used_ram}MB"
 
-# Get total storage of root
-total_storage=$(df -h | grep -oP "^.*\/dev\/mapper\/ubuntu--vg-ubuntu--lv.*$" | awk '{print $2}')
+# Get total storage of root partition
+total_bytes=$(df --block-size=1 / | awk 'NR==2 {print $2}')
+total_storage=$(echo "scale=2; $total_bytes / (1024 * 1024 * 1024)" | bc)
 
-echo "Total Storage: ${total_storage}"
+echo "Total Storage: ${total_storage}GB (root)"
 
 # Get total used memory
 total_used_memory=$(docker stats --no-stream --format "{{.MemUsage}}" $(docker ps -q) | awk '{sum += $1} END {print sum}')
 
 echo "Total Used Memory: ${total_used_memory}"
 
-# Get total used storage of system /dev/mapper/ubuntu--vg-ubuntu--lv
-total_used_storage=$(df -h | grep -oP "^.*\/dev\/mapper\/ubuntu--vg-ubuntu--lv.*$" | awk '{print $3}')
+# Get total used storage
+total_bytes_used=$(df --block-size=1 / | awk 'NR==2 {print $3}')
+total_used_storage=$(echo "scale=2; $total_bytes_used / (1024 * 1024 * 1024)" | bc)
 
-echo "Total Used Storage: ${total_used_storage}"
+echo "Total Used Storage: ${total_used_storage} (root)"
 
 # Line with the hostname
 echo "-----------------------"
@@ -37,7 +41,6 @@ echo "-----------------------"
 echo ""
 echo "Docker Containers"
 echo "-----------------------"
-
 
 # Get a list of running Docker container IDs
 container_ids=$(docker ps -q)
@@ -53,13 +56,9 @@ for container_id in $container_ids; do
     # Get container memory usage
     mem_usage=$(docker stats --no-stream --format "table {{.MemUsage}}" $container_id | tail -n 1)
 
-    # # Get storage usage of container
-    # storage_usage=$(docker inspect --format='{{json .GraphDriver.Data}}' $container_id | jq -r '.DeviceSize')
-
     # Print the results
     echo "Container: $container_name"
     echo "CPU Usage: $cpu_usage"
     echo "Memory Usage: $mem_usage"
-    # echo "Storage Usage: $storage_usage"
     echo "-----------------------"
 done
